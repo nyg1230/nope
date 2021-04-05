c9427426ab3bc5be6891890dfa05dc19e04008a0
nyg1230 <44147752+nyg1230@users.noreply.github.com>
2021-04-05 21:40:27 +0900

    Initial commit

---
+++ src/main/java/com/pjt/nope/nope/NopeApplication.java
@@ -0,0 +1,13 @@
+package com.pjt.nope.nope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjt/nope/nope/NopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt.nope.nope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
